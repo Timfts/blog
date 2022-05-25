37a3c43c362f957e0d3eea92ea2c5ecb7b9ed48e
Italo Fontes <italo5123@hotmail.com>
2022-05-25 19:51:39 -0300

    chore: :art: replicated tv effect shader from reference

---
+++ src/components/MainHero.controller.ts
@@ -0,0 +1,18 @@
+import "shader-doodle";
+
+const text = "L  O  R  E  M     I  P  S  U  M";
+const canvas = document.getElementById("canvas") as HTMLCanvasElement;
+console.log(canvas);
+canvas.height = 1024;
+canvas.width = 1024;
+const ctx = canvas.getContext("2d");
+ctx.font = "Bold " + canvas.width / 12 + "px 'Helvetica'";
+ctx.textAlign = "center";
+ctx.textBaseline = "top";
+ctx.fillStyle = "#000";
+ctx.fillRect(0, 0, canvas.width, canvas.height);
+ctx.fillStyle = "#fff";
+ctx.translate(canvas.width / 2, 0);
+for (var i = -1; i < 6; i++) {
+  ctx.fillText(text, 0, (i * canvas.height) / 6);
+}
