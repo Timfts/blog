a1f2425f775a26be0cab197c46a15f793ae12f54
Italo Fontes <italo5123@hotmail.com>
2022-05-25 22:09:06 -0300

    chore: :alembic: applied shader over image

--- src/components/MainHero.controller.ts
+++ src/components/MainHero.controller.ts
@@ -9,9 +9,9 @@ const ctx = canvas.getContext("2d");
 ctx.font = "Bold " + canvas.width / 12 + "px 'Helvetica'";
 ctx.textAlign = "center";
 ctx.textBaseline = "top";
-ctx.fillStyle = "#000";
-ctx.fillRect(0, 0, canvas.width, canvas.height);
 ctx.fillStyle = "#fff";
+ctx.fillRect(0, 0, canvas.width, canvas.height);
+ctx.fillStyle = "#000";
 ctx.translate(canvas.width / 2, 0);
 for (var i = -1; i < 6; i++) {
   ctx.fillText(text, 0, (i * canvas.height) / 6);
