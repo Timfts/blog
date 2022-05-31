e6dccc021e726de486447c8d043f1987e1fd4cb3
Tim Fontes <italo5123@hotmail.com>
2022-05-31 15:54:10 -0300

    feat/adding-experiments

--- src/components/MainHero.controller.ts
+++ src/components/MainHero.controller.ts
@@ -1,17 +1,17 @@
 import "shader-doodle";
 
-const text = "L  O  R  E  M     I  P  S  U  M";
+const text = "Teste Batata";
 const canvas = document.getElementById("canvas") as HTMLCanvasElement;
 console.log(canvas);
 canvas.height = 1024;
 canvas.width = 1024;
 const ctx = canvas.getContext("2d");
-ctx.font = "Bold " + canvas.width / 12 + "px 'Helvetica'";
+ctx.font = "50px 'Pixel Operator Mono'";
 ctx.textAlign = "center";
 ctx.textBaseline = "top";
-ctx.fillStyle = "#fff";
-ctx.fillRect(0, 0, canvas.width, canvas.height);
 ctx.fillStyle = "#000";
+ctx.fillRect(0, 0, canvas.width, canvas.height);
+ctx.fillStyle = "#fff";
 ctx.translate(canvas.width / 2, 0);
 for (var i = -1; i < 6; i++) {
   ctx.fillText(text, 0, (i * canvas.height) / 6);
