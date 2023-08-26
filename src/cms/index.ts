09c7d4a8f13722a9452376402df22dd25a27f795
Tim Fontes <italo5123@hotmail.com>
2023-08-26 17:11:39 -0300

    chore: :memo: added commit patter to decap cms

--- src/cms/index.ts
+++ src/cms/index.ts
@@ -2,12 +2,22 @@ import postTopics from "./collections/posts-topics";
 import learnings from "./collections/learnings";
 import snippetsCategories from "./collections/snippets-categories";
 import posts from "./collections/posts";
+import { SITE_URL } from "../constants";
 
 const decapCMSConfig = {
   config: {
+    site_url: SITE_URL,
+    display_url: SITE_URL,
     backend: {
       name: "git-gateway",
       branch: "main",
+      commit_messages: {
+        create: "chore: :memo: Create {{collection}} “{{slug}}”",
+        update: "chore: :memo: Update {{collection}} “{{slug}}”",
+        delete: "chore: :memo: Delete {{collection}} “{{slug}}”",
+        uploadMedia: "chore: :memo: Upload “{{path}}”",
+        deleteMedia: "chore: :memo: Delete “{{path}}”",
+      },
     },
     publish_mode: "editorial_workflow",
     local_backend: true,
