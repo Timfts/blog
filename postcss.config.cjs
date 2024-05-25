f253302c90f8db2857c595327fbd3f70476e3031
Tim Fontes <italo5123@hotmail.com>
2024-05-25 19:22:32 -0300

    feat: :sparkles: added new inline images plugin

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,9 +1,17 @@
+const path = require("path");
+const autoprefixer = require("autoprefixer");
+const postcssUrl = require("postcss-url");
+const cssNano = require("cssnano");
+
+const inlineUrl = postcssUrl({
+  filter: (filename) => {
+    const file = filename?.url || "";
+    return file.endsWith("png") || file.endsWith("svg");
+  },
+  url: "inline",
+  basePath: path.resolve("public"),
+});
+
 module.exports = {
-    plugins: [
-      require('autoprefixer'),
-      require('postcss-inline-svg')({
-        paths: ['public']
-      }),
-      require('cssnano'),
-    ],
-  };
+  plugins: [autoprefixer, inlineUrl, cssNano],
+};
