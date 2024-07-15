4187cc10c91aaa5a5d2574d3ffd86e75f4154195
Tim Fontes <italo5123@hotmail.com>
2024-07-14 22:52:20 -0300

    feat: :sparkles: posts i18n working

---
+++ src/content/helpers.ts
@@ -0,0 +1,64 @@
+import { CollectionEntry, getCollection } from "astro:content";
+
+type PostVersionMap = {
+  [slug: string]: CollectionEntry<"posts">;
+};
+
+type PostsMap = {
+  [slug: string]: PostVersionMap;
+};
+
+/**
+ * Creates a map that groups different language versions of the same post
+ * under the same slug
+ */
+export function getPostsMap(allPosts: CollectionEntry<"posts">[]) {
+  const allPostsMap: PostsMap = allPosts.reduce((acc, entry) => {
+    const [lang, postID] = entry.slug.split("/");
+    const postEntry = acc[postID];
+    const newPostEntry = postEntry
+      ? { ...postEntry, [lang]: entry }
+      : { [lang]: entry };
+
+    return { ...acc, [postID]: newPostEntry };
+  }, {});
+
+  return allPostsMap;
+}
+
+export function getI18NPostMap(
+  currentLang: string,
+  postVersions: PostVersionMap
+): I18NMap {
+  const postLangs = Object.keys(postVersions);
+  let pathsMap: Record<string, string> = {};
+
+  postLangs.forEach((lang) => {
+    const rootPath = lang === "en" ? "" : `/${lang}`;
+    const versionUrl = postVersions[lang]?.data?.page_url;
+    pathsMap[lang] = `${rootPath}/post/${versionUrl}`;
+  });
+
+  return {
+    currentLang,
+    paths: pathsMap,
+  };
+}
+
+export async function getPostsPathsByLanguage(lang: Lang) {
+  const blogPosts = await getCollection("posts");
+  const allPostsMap = getPostsMap(blogPosts);
+  const ptPosts = blogPosts.filter((post) => {
+    return post.slug.startsWith(`${lang}/`);
+  });
+
+  return ptPosts.map((entry) => {
+    const [, rawPostSlug] = entry.slug.split("/");
+    const postEntries = allPostsMap[rawPostSlug];
+
+    return {
+      params: { path: entry.data.page_url },
+      props: { entries: postEntries },
+    };
+  });
+}
--- src/content/posts/pt-br/teste-cenoura.md
+++ src/content/posts/pt-br/teste-cenoura.md
@@ -1,4 +1,5 @@
 ---
+title: teste cenoura
 enabled: true
 page_url: lulu-hehe
 created_date: 2023-08-28T01:54:28.792Z
