33be4d94604b2491ca50cc105701d19a685e5895
Tim Fontes <italo5123@hotmail.com>
2024-12-23 19:05:14 -0300

    feat: :construction: commiting missing files

--- src/templates/projects/i18n.ts
+++ src/templates/projects/i18n.ts
@@ -1,9 +1,13 @@
 const i18n = {
   en: {
     title: "Projects",
+    featuredTitle: "Featured",
+    allProjects: "All projects",
   },
   "pt-br": {
     title: "Projetos",
+    featuredTitle: "Destaques",
+    allProjects: "Todos projetos",
   },
 };
 
