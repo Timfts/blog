8a6c28b504d5d990bd1fed41869e277f41829a2d
Tim Fontes <italo5123@hotmail.com>
2025-01-12 20:31:53 -0300

    feat: :sparkles: seo setup

--- src/helpers/date.ts
+++ src/helpers/date.ts
@@ -4,3 +4,11 @@ export const fmtDate = (date: Date, locale: string) =>
     month: "long",
     year: "numeric",
   });
+
+export function fmtYYYYMMDD(date: Date) {
+  const year = date.getFullYear();
+  const month = String(date.getMonth() + 1).padStart(2, "0");
+  const day = String(date.getDate()).padStart(2, "0");
+
+  return `${year}-${month}-${day}`;
+}
