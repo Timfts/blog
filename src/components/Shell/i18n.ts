d2f17dc0e9190b493c43847b6eb70fbeb3367aca
Tim Fontes <italo5123@hotmail.com>
2025-01-12 20:01:59 -0300

    feat: :sparkles: added some SEO features

--- src/components/Shell/i18n.ts
+++ src/components/Shell/i18n.ts
@@ -1,9 +1,11 @@
 const i18n = {
   en: {
     htmlLang: "en",
+    metaDescription: "Website meta description", //TODO
   },
   "pt-br": {
     htmlLang: "pt-BR",
+    metaDescription: "Website meta description",
   },
 };
 
