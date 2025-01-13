5738433b04a38d6d5cca0031b47293cd9f5960c7
Tim Fontes <italo5123@hotmail.com>
2025-01-12 21:34:08 -0300

    feat: :sparkles: added loading lazy to work entries

--- src/components/PostsList/i18n.ts
+++ src/components/PostsList/i18n.ts
@@ -2,10 +2,12 @@ const i18n = {
   en: {
     topicsTitle: "topics",
     searchPlaceholder: "Search",
+    searchAriaLabel: "Search posts"
   },
   "pt-br": {
     topicsTitle: "topicos",
     searchPlaceholder: "Buscar",
+    searchAriaLabel: "Buscar posts"
   },
 };
 
