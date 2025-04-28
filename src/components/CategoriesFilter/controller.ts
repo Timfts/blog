6204cb6c9e783c5aa72258b571f0ee4fb08c4616
Tim Fontes <italo5123@hotmail.com>
2025-04-28 00:31:10 -0300

    feat: :sparkles: added topics filter to posts

---
+++ src/components/CategoriesFilter/controller.ts
@@ -0,0 +1,46 @@
+import { getSearchQuery } from "@helpers/navigation";
+import elementController from "@lib/elementController";
+
+elementController(
+  "categories-filter",
+  ({ query }) => {
+    const filterBtn = query("[data-filter]") as HTMLButtonElement;
+    const filtersForm = query("[data-filter-form]") as HTMLFormElement;
+    const showFilterTxt = filterBtn.getAttribute("data-show");
+    const hideFilterTxt = filterBtn.getAttribute("data-hide");
+    const searchQuery: string | undefined = filterBtn.getAttribute("data-search-query");
+    let isShowingFilters = false;
+
+    initialize();
+
+    function initialize() {
+      filterBtn.onclick = toggleFilters;
+      checkQuery();
+    }
+
+    function checkQuery() {
+      if (!searchQuery) return;
+      const openWithQuery = getSearchQuery(searchQuery);
+      if (!!openWithQuery) {
+        showFilters();
+      }
+    }
+
+    function toggleFilters() {
+      isShowingFilters ? hideFilters() : showFilters();
+    }
+
+    function showFilters() {
+      filterBtn.innerHTML = hideFilterTxt;
+      filtersForm.style.display = "block";
+      isShowingFilters = true;
+    }
+
+    function hideFilters() {
+      filterBtn.innerHTML = showFilterTxt;
+      filtersForm.style.display = "none";
+      isShowingFilters = false;
+    }
+  },
+  { rerun: "sameroute" }
+);
