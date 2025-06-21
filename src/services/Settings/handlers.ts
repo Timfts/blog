aef5b32a37a708fc9a30ffc0b5b64d7862e56a8c
Tim Fontes <italo5123@hotmail.com>
2025-06-20 22:48:39 -0300

    feat: :sparkles: added theme color toggler

--- src/services/Settings/handlers.ts
+++ src/services/Settings/handlers.ts
@@ -1,22 +1,16 @@
 import { getCurrentLocale, redirectToLocalizedPage } from "@helpers/i18n";
 
-import {
-  clearVideoBackground,
-  createVideoBackground,
-  loadPublicCSSFile,
-} from "./helpers";
+import { clearVideoBackground, createVideoBackground, loadPublicCSSFile } from "./helpers";
 
 const prefsHandlers = {
   filter: (option: string) => {
     if (option !== "none") loadPublicCSSFile("filters.css");
   },
 
-  "background": (value) => {
+  background: (value) => {
     const videoBackgrounds = { matrix: "/videos/matrix-background.mp4" };
     const videoBackground = videoBackgrounds[value];
-    videoBackground
-      ? createVideoBackground(videoBackground)
-      : clearVideoBackground();
+    videoBackground ? createVideoBackground(videoBackground) : clearVideoBackground();
   },
 
   lang: (lang: Lang) => {
@@ -24,6 +18,15 @@ const prefsHandlers = {
     if (!lang || alreadyOnDefinedLang) return;
 
     redirectToLocalizedPage(lang as Lang);
+  },
+
+  theme: (theme: string) => {
+    const themeColor = {
+      default: "#d3e5fa",
+      dark: "#404040",
+    }[theme];
+    const themeMeta = document.querySelector("meta[name=theme-color]");
+    themeMeta.setAttribute("content", themeColor);
   },
 };
 
